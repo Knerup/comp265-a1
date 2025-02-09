6802660d3dd3db3dafc8856c9cbfea500a911d1b
Knerup <kfranzis.pastor@gmail.com>
2025-02-09 12:14:28 -0600

    RePasted my code for Exercise 1 of comp265:
    - Chosen Recipes are Pizza, Chicken Alfredo, Dog Treats
    - Added base spacing around the texts for styling

--- App.js
+++ App.js
@@ -1,58 +1,86 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, ScrollView } from 'react-native';
+import { ScrollView, StyleSheet, Text, View, Button } from 'react-native';
 import SegmentedControl from '@react-native-segmented-control/segmented-control';
 
 export default function App() {
-  const [unit, setUnit] = useState('C'); // 'C' for Celsius, 'F' for Fahrenheit
-  const [selectedCity, setSelectedCity] = useState('Saskatoon'); // Default city
-  const [selectedIndex, setSelectedIndex] = useState(0);
 
-  // Static weather data
-  const weatherData = [
-    { city: 'Saskatoon', temperatureC: 22, condition: 'Sunny' },
-    { city: 'Regina', temperatureC: 19, condition: 'Cloudy' },
-    { city: 'Prince Albert', temperatureC: 16, condition: 'Rainy' },
+  const [selectedFood, setSelectedFood] = useState('Pizza'); // Default city
+
+  // Static Recipe Data
+  // Recipes from: https://www.allrecipes.com/recipe/20171/quick-and-easy-pizza-crust/, https://www.allrecipes.com/recipe/276725/creamy-chicken-alfredo/, https://www.allrecipes.com/recipe/133123/peanut-butter-and-pumpkin-dog-treats/
+  const recipeData = [
+    {
+      recipe: 'Pizza',
+      description: "If you're looking for a homemade pizza crust recipe that's great for beginners, you're in luck. This top-rated recipe is super easy to throw together on a whim and it puts the store-bought stuff to shame.",
+      ingredients: "1 cup warm water (110 degrees F/45 degrees C) | 1 (.25 ounce) package active dry yeast | 1 teaspoon white sugar | 2 ½ cups bread flour | 2 tablespoons olive oil | 1 teaspoon salt",
+      instructions: "Step 1: Gather all ingredients. Preheat oven to 450 degrees F (230 degrees C), and lightly grease a pizza pan. | Step 2: Place warm water in a bowl; add yeast and sugar. Mix and let stand until creamy, about 10 minutes. | Step 3: Add flour, oil, and salt to the yeast mixture; beat until smooth. You can do this by hand or use a stand mixer fitted with a dough hook to make it easier. Let this rest for 5 minutes. | Step 4: Turn dough out onto a lightly floured surface and pat or roll into a 12-inch circle. Transfer this to the prepared pizza pan. | Step 5: Spread crust with sauce and toppings of your choice. Bake in the preheated oven until golden brown, 15 to 20 minutes. Remove from the oven and let cool for 5 minutes before serving."
+    },
+
+    {
+      recipe: 'Chicken Alfredo',
+      description: "This recipe shows you how to take plain, bland, store-made sauce and make it more than restaurant-worthy.",
+      ingredients: "*2 pounds skinless, boneless chicken breasts, cut into chunks | *Salt and ground black pepper to taste | *1 teaspoon white sugar |*2 ½ cups bread flour | *2 tablespoons olive oil | *1 teaspoon salt",
+      instructions: "Step 1: Gather all ingredients. Preheat oven to 450 degrees F (230 degrees C), and lightly grease a pizza pan. | Step 2: Place warm water in a bowl; add yeast and sugar. Mix and let stand until creamy, about 10 minutes. | Step 3: Add flour, oil, and salt to the yeast mixture; beat until smooth. You can do this by hand or use a stand mixer fitted with a dough hook to make it easier. Let this rest for 5 minutes. | Step 4: Turn dough out onto a lightly floured surface and pat or roll into a 12-inch circle. Transfer this to the prepared pizza pan. | Step 5: Spread crust with sauce and toppings of your choice. Bake in the preheated oven until golden brown, 15 to 20 minutes. Remove from the oven and let cool for 5 minutes before serving."
+    },
+
+
+    {
+      recipe: 'Dog Treats',
+      description: "If you're looking for a homemade pizza crust recipe that's great for beginners, you're in luck. This top-rated recipe is super easy to throw together on a whim and it puts the store-bought stuff to shame.",
+      ingredients: "*1 cup warm water (110 degrees F/45 degrees C) | *1 (.25 ounce) package active dry yeast | *1 teaspoon white sugar |*2 ½ cups bread flour | *2 tablespoons olive oil | *1 teaspoon salt",
+      instructions: "Step 1: Gather all ingredients. Preheat oven to 450 degrees F (230 degrees C), and lightly grease a pizza pan. | Step 2: Place warm water in a bowl; add yeast and sugar. Mix and let stand until creamy, about 10 minutes. | Step 3: Add flour, oil, and salt to the yeast mixture; beat until smooth. You can do this by hand or use a stand mixer fitted with a dough hook to make it easier. Let this rest for 5 minutes. | Step 4: Turn dough out onto a lightly floured surface and pat or roll into a 12-inch circle. Transfer this to the prepared pizza pan. | Step 5: Spread crust with sauce and toppings of your choice. Bake in the preheated oven until golden brown, 15 to 20 minutes. Remove from the oven and let cool for 5 minutes before serving."
+    },
   ];
 
-  const toggleUnit = () => {
-    setUnit((prevUnit) => (prevUnit === 'C' ? 'F' : 'C'));
+  const handleRecipeChange = (event) => {
+    console.log(event.value);
+    setSelectedFood(event.value);
   };
 
-  const convertTemperature = (tempC) => {
-    return unit === 'C' ? tempC : (tempC * 9) / 5 + 32;
-  };
+  const selectedRecipe = recipeData.find((data) => data.recipe === selectedFood);
 
-  const handleCityChange = (event) => {
-    setSelectedIndex(event.nativeEvent.selectedSegmentIndex);
-    setSelectedCity(event.nativeEvent.value);
-  };
-
-  const selectedWeather = weatherData.find((data) => data.city === selectedCity);
 
   return (
     <ScrollView>
-    <View style={styles.container}>
-      <Text style={styles.header}>Weather App</Text>
-      <Button
-        onPress={toggleUnit}
-        title={`Toggle to ${unit === 'C' ? 'Fahrenheit' : 'Celsius'}`}
-        accessibilityLabel="Toggle Temperature Unit"
-      />
+      <View style={styles.container}>
+        {/* Text for my title texts and subtitle */}
+        <Text style={styles.titleText}>Assignment #1: Single-Screen Mobile Application</Text>
+        <Text style={styles.subtitleText}>By: Knerup Pastor | COMP 265</Text>
 
-      <SegmentedControl
-        values={['Saskatoon', 'Regina', 'Prince Albert']}
-        selectedIndex={selectedIndex}
-        onChange={handleCityChange}
-      />
+        <SegmentedControl
+          style={styles.toggleButton}
+          values={['Pizza', 'Chicken Alfredo', 'Dog Treats']}
+          selectedIndex={selectedFood}
+          onChange={(event) => {
+            handleRecipeChange(event.nativeEvent);
+          }}
+        />
 
-      <View style={styles.details}>
-        <Text>{selectedWeather.city}</Text>
-        <Text>{selectedWeather.condition}</Text>
-        <Text>{convertTemperature(selectedWeather.temperatureC)}°{unit}</Text>
+
+        {/* Displaying the recipe for the user */}
+        <View style={styles.recipeName}>
+          {/* The Recipe Name */}
+          <Text>{selectedRecipe.recipe}</Text>
+        </View>
+
+        <View style={styles.recipeDescriptions}>
+          {/* The Recipe Description */}
+          <Text>{selectedRecipe.description}</Text>
+        </View>
+
+        <View style={styles.recipeIngredients}>
+          {/* The Recipe Ingredients */}
+          <Text>{selectedRecipe.ingredients}</Text>
+        </View>
+
+        <View style={styles.recipeIngredients}>
+          {/* The Recipe Instructions */}
+          <Text>{selectedRecipe.instructions}</Text>
+        </View>
+
+        <StatusBar style="auto" />
       </View>
-      <StatusBar style="auto" />
-    </View>
     </ScrollView>
   );
 }
@@ -61,19 +89,39 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    // alignItems: 'center',
     justifyContent: 'center',
     padding: 30,
   },
-  header: {
-    fontSize: 30,
+  titleText: {
+    marginTop: 20,
+    fontSize: 20,
     fontWeight: 'bold',
+    textAlign: 'center'
   },
-  details: {
-    borderStyle: 'solid',
-    borderColor: 'black',
-    borderWidth: 2,
-    borderRadius: 6,
-    padding: 4,
-    margin: 10,
-  }
+  subtitleText: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    paddingBottom: 30,
+  },
+  toggleButton: {
+    marginBottom: 30,
+  },
+  recipeName: {
+    paddingBottom: 30,
+    padding: 20,
+  },
+  recipeDescriptions: {
+    textAlign: 'center',
+    padding: 20,
+  },
+  recipeIngredients: {
+    textAlign: 'center',
+    padding: 20,
+  },
+  recipeInstructions: {
+    textAlign: 'center',
+    padding: 20,
+  },
 });
